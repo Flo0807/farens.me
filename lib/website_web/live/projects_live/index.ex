e450e5c6d30588f2bd306954b398de66b7c6df86
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2022-09-18 19:29:15 +0200

    Add projects live view

---
+++ lib/website_web/live/projects_live/index.ex
@@ -0,0 +1,15 @@
+defmodule WebsiteWeb.ProjectsLive.Index do
+  use WebsiteWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(_params, url, socket) do
+    socket =
+      socket
+      |> assign(:url, url)
+
+    {:noreply, socket}
+  end
+end
