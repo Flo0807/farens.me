211fc20a4ae6b579167d92c62c1039f2c0e79f76
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2022-09-18 19:28:50 +0200

    Add home live view

--- lib/website_web/live/home_live/index.ex
+++ lib/website_web/live/home_live/index.ex
@@ -1,7 +1,25 @@
 defmodule WebsiteWeb.HomeLive.Index do
   use WebsiteWeb, :live_view
 
+  alias Website.Repo
+
   def mount(_params, _session, socket) do
     {:ok, socket}
+  end
+
+  def handle_params(_params, url, socket) do
+    {:ok, articles} = Repo.list(:articles)
+
+    articles =
+      articles
+      |> Enum.shuffle()
+      |> Enum.take(3)
+
+    socket =
+      socket
+      |> assign(:url, url)
+      |> assign(:articles, articles)
+
+    {:noreply, socket}
   end
 end
