4e6f27477a8548a889ff20cd860b545688247161
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2022-09-29 17:40:51 +0200

    Add locals without parens to .formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,14 @@
+locals_without_parens = [
+  attr: 2,
+  attr: 3,
+  slot: 1,
+  slot: 2,
+  slot: 3
+]
+
 [
   import_deps: [:phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  locals_without_parens: locals_without_parens
 ]
