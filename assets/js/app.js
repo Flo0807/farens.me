0b4113e3d5c3ed9ef598350cff0d7fbd60fc68e0
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2022-10-06 23:55:23 +0200

    Add alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,11 +22,22 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import hooks from "./hooks";
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+Alpine.start()
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   params: { _csrf_token: csrfToken },
-  hooks
+  hooks,
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from._x_dataStack) {
+        window.Alpine.clone(from, to)
+      }
+    }
+  },
 })
 
 // Show progress bar on live navigation and form submits
