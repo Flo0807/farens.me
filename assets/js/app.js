84bdf0ab0ac051ec547f404598ce5bdd1cb1712e
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2023-02-04 11:42:50 +0100

    Install topbar via npm

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import topbar from "topbar"
 import hooks from "./hooks";
 import Alpine from 'alpinejs'
 
