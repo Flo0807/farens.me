27a95ab52a731963b8ba1e0350d94899df3e5f65
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2023-05-13 00:58:32 +0200

    Redesign pages

--- lib/website_web/components/core_components.ex
+++ lib/website_web/components/core_components.ex
@@ -13,58 +13,54 @@ defmodule WebsiteWeb.CoreComponents do
     %{to: "/projects", label: "Projects"}
   ]
 
-  def merge_class(%{class: class}, default), do: class <> " " <> default
-  def merge_class(_, default), do: default
-
   attr(:url, :string, required: true, doc: "The current url.")
   attr(:nav_links, :list, default: @nav_links, doc: "A list of nav links to be rendered.")
+  attr(:class, :string, default: nil, doc: "A class to be added to the header.")
 
   def header(assigns) do
     ~H"""
     <div x-data="{ mobile_menu: false }">
-      <header class="flex justify-center sm:px-8 lg:px-16">
-        <div class="flex w-full max-w-7xl items-center justify-between px-8 pt-6 sm:px-10 lg:px-16">
-          <.link navigate="/" class="group shadow-zinc-800/5 shadow-lg">
-            <img
-              class="inline-block h-8 w-8 rounded-full ring-2 ring-white group-hover:ring-cyan-400"
-              src="/images/me.jpg"
-              alt="logo"
-            />
-          </.link>
-          <nav class="bg-white/90 ring-zinc-900/5 shadow-zinc-800/5 hidden rounded-full px-6 text-zinc-800 shadow-lg ring-1 dark:ring-white/10 dark:bg-zinc-800 dark:text-zinc-100 md:block">
-            <ul class="flex space-x-6 font-medium">
-              <li
-                :for={%{to: to, label: label} <- @nav_links}
-                class={[
-                  "relative px-3 py-2",
-                  if(active?(@url, to),
-                    do: "text-cyan-500 dark:text-cyan-400",
-                    else: "hover:text-cyan-500 hover:dark:text-cyan-400"
-                  )
-                ]}
+      <header class={["flex items-center justify-between", @class]}>
+        <.link navigate="/" class="group shadow-zinc-800/5 shadow-lg">
+          <img
+            class="inline-block h-8 w-8 rounded-full ring-2 ring-white group-hover:ring-cyan-400"
+            src="/images/me.jpg"
+            alt="Portrait of Florian Arens"
+          />
+        </.link>
+        <nav class="bg-white/90 ring-zinc-900/5 shadow-zinc-800/5 hidden rounded-full px-6 text-zinc-800 shadow-lg ring-1 dark:ring-white/10 dark:bg-zinc-800 dark:text-zinc-100 md:block">
+          <ul class="flex space-x-6 font-medium">
+            <li
+              :for={%{to: to, label: label} <- @nav_links}
+              class={[
+                "relative px-3 py-2",
+                if(active?(@url, to),
+                  do: "text-cyan-500 dark:text-cyan-400",
+                  else: "hover:text-cyan-500 hover:dark:text-cyan-400"
+                )
+              ]}
+            >
+              <.link navigate={to}>
+                <%= label %>
+              </.link>
+              <span
+                :if={active?(@url, to)}
+                class="from-cyan-400/0 via-cyan-500/80 to-cyan-500/0 absolute inset-x-1 -bottom-px h-px bg-gradient-to-r dark:via-cyan-400/80 dark:to-cyan-400/0"
               >
-                <.link navigate={to}>
-                  <%= label %>
-                </.link>
-                <span
-                  :if={active?(@url, to)}
-                  class="from-cyan-400/0 via-cyan-500/80 to-cyan-500/0 absolute inset-x-1 -bottom-px h-px bg-gradient-to-r dark:via-cyan-400/80 dark:to-cyan-400/0"
-                >
-                </span>
-              </li>
-            </ul>
-          </nav>
+              </span>
+            </li>
+          </ul>
+        </nav>
 
-          <div
-            class="bg-white/90 ring-zinc-900/5 shadow-zinc-800/5 flex cursor-pointer items-center space-x-2 rounded-full px-4 py-2 text-zinc-800 shadow-lg ring-1 dark:ring-white/10 dark:bg-zinc-800 dark:text-zinc-100 md:hidden"
-            @click="mobile_menu = true"
-          >
-            <p>Menu</p>
-            <Heroicons.chevron_down solid class="h-5 w-5" />
-          </div>
-
-          <.color_scheme_switch />
+        <div
+          class="bg-white/90 ring-zinc-900/5 shadow-zinc-800/5 flex cursor-pointer items-center space-x-2 rounded-full px-4 py-2 text-zinc-800 shadow-lg ring-1 dark:ring-white/10 dark:bg-zinc-800 dark:text-zinc-100 md:hidden"
+          @click="mobile_menu = true"
+        >
+          <p>Menu</p>
+          <Heroicons.chevron_down solid class="h-5 w-5" />
         </div>
+
+        <.color_scheme_switch />
 
         <.mobile_nav />
       </header>
@@ -124,72 +120,25 @@ defmodule WebsiteWeb.CoreComponents do
 
   def footer(assigns) do
     ~H"""
-    <footer class={["flex justify-center px-8 sm:px-10 lg:px-16", @class]}>
-      <div class="w-full max-w-7xl border-t border-zinc-300 pt-8 pb-16 dark:border-zinc-700">
-        <div class="flex flex-col items-center space-y-2 px-16 sm:justify-between md:flex-row md:space-y-0">
-          <nav class="text-zinc-800 dark:text-zinc-100">
-            <ul class="flex space-x-4 font-medium">
-              <li :for={%{to: to, label: label} <- @nav_links}>
-                <.link
-                  navigate={to}
-                  class={if active?(@url, to), do: "text-cyan-500 dark:text-cyan-400", else: ""}
-                >
-                  <%= label %>
-                </.link>
-              </li>
-            </ul>
-          </nav>
-          <p class="text-center text-zinc-400 dark:text-zinc-500">
-            © 2022 Florian Arens. All rights reserved.
-          </p>
-        </div>
+    <footer class={["border-t border-zinc-300 dark:border-zinc-700", @class]}>
+      <div class="flex flex-col items-center space-y-2 sm:justify-between md:flex-row md:space-y-0">
+        <nav class="text-zinc-800 dark:text-zinc-100">
+          <ul class="flex space-x-4 font-medium">
+            <li :for={%{to: to, label: label} <- @nav_links}>
+              <.link
+                navigate={to}
+                class={if active?(@url, to), do: "text-cyan-500 dark:text-cyan-400", else: ""}
+              >
+                <%= label %>
+              </.link>
+            </li>
+          </ul>
+        </nav>
+        <p class="text-center text-zinc-400 dark:text-zinc-500">
+          © 2022 Florian Arens. All rights reserved.
+        </p>
       </div>
     </footer>
-    """
-  end
-
-  attr(:title, :string, required: true, doc: "The title.")
-  attr(:logo, :string, required: true, doc: "The logo src.")
-  attr(:description, :string, required: true, doc: "The description.")
-  attr(:date_range, :string, required: true, doc: "The date range.")
-
-  def cv_item(assigns) do
-    ~H"""
-    <div class="flex space-x-6 pt-2">
-      <img class="mt-2 inline-block h-8 w-8 rounded-full ring-2 ring-zinc-100" src={@logo} alt="logo" />
-      <div class="flex w-full flex-col space-y-1">
-        <p class="text-md font-medium text-zinc-800 dark:text-zinc-100">
-          <%= @title %>
-        </p>
-        <div class="flex justify-between space-x-4">
-          <div class="flex w-3/4 flex-col space-y-2 text-sm text-zinc-600 dark:text-zinc-400">
-            <p><%= @description %></p>
-          </div>
-          <div class="self-end" role="tooltip" aria-label={@date_range} data-balloon-pos="up">
-            <Heroicons.calendar_days class="h-5 w-5 text-zinc-600 dark:text-zinc-400" />
-          </div>
-        </div>
-      </div>
-    </div>
-    """
-  end
-
-  attr(:label, :string, required: true, doc: "The label to be displayed.")
-
-  slot(:icon, doc: "The icon.")
-  slot(:inner_block)
-
-  def cv_container(assigns) do
-    ~H"""
-    <div class="bg-zinc-200/30 flex flex-col space-y-3 rounded-xl px-6 py-4 dark:bg-zinc-800/30">
-      <div class="text-md flex items-center space-x-4 font-medium">
-        <%= render_slot(@icon) %>
-        <p class="text-zinc-800 dark:text-zinc-100">
-          <%= @label %>
-        </p>
-      </div>
-      <%= render_slot(@inner_block) %>
-    </div>
     """
   end
 
@@ -222,7 +171,6 @@ defmodule WebsiteWeb.CoreComponents do
     """
   end
 
-  attr(:socket, :map, required: true, doc: "The socket.")
   attr(:articles, :list, required: true, doc: "A list of articles.")
 
   def article_timeline(assigns) do
@@ -296,18 +244,12 @@ defmodule WebsiteWeb.CoreComponents do
     ~H"""
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class={["icon icon-tabler icon-tabler-brand-github", @class]}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      viewBox="0 0 64 64"
+      width="32px"
+      height="32px"
+      class={@class}
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+      <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z" />
     </svg>
     """
   end
@@ -318,22 +260,28 @@ defmodule WebsiteWeb.CoreComponents do
     ~H"""
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class={["icon icon-tabler icon-tabler-brand-linkedin", @class]}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      viewBox="0 0 50 50"
+      width="32px"
+      height="32px"
+      class={@class}
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <rect x="4" y="4" width="16" height="16" rx="2" />
-      <line x1="8" y1="11" x2="8" y2="16" />
-      <line x1="8" y1="8" x2="8" y2="8.01" />
-      <line x1="12" y1="16" x2="12" y2="11" />
-      <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+      <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z" />
+    </svg>
+    """
+  end
+
+  attr(:class, :string, default: nil, doc: "Additional classes to be added to the icon.")
+
+  def twitter_icon(assigns) do
+    ~H"""
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      width="32px"
+      height="32px"
+      class={@class}
+    >
+      <path d="M 50.0625 10.4375 C 48.214844 11.257813 46.234375 11.808594 44.152344 12.058594 C 46.277344 10.785156 47.910156 8.769531 48.675781 6.371094 C 46.691406 7.546875 44.484375 8.402344 42.144531 8.863281 C 40.269531 6.863281 37.597656 5.617188 34.640625 5.617188 C 28.960938 5.617188 24.355469 10.21875 24.355469 15.898438 C 24.355469 16.703125 24.449219 17.488281 24.625 18.242188 C 16.078125 17.8125 8.503906 13.71875 3.429688 7.496094 C 2.542969 9.019531 2.039063 10.785156 2.039063 12.667969 C 2.039063 16.234375 3.851563 19.382813 6.613281 21.230469 C 4.925781 21.175781 3.339844 20.710938 1.953125 19.941406 C 1.953125 19.984375 1.953125 20.027344 1.953125 20.070313 C 1.953125 25.054688 5.5 29.207031 10.199219 30.15625 C 9.339844 30.390625 8.429688 30.515625 7.492188 30.515625 C 6.828125 30.515625 6.183594 30.453125 5.554688 30.328125 C 6.867188 34.410156 10.664063 37.390625 15.160156 37.472656 C 11.644531 40.230469 7.210938 41.871094 2.390625 41.871094 C 1.558594 41.871094 0.742188 41.824219 -0.0585938 41.726563 C 4.488281 44.648438 9.894531 46.347656 15.703125 46.347656 C 34.617188 46.347656 44.960938 30.679688 44.960938 17.09375 C 44.960938 16.648438 44.949219 16.199219 44.933594 15.761719 C 46.941406 14.3125 48.683594 12.5 50.0625 10.4375 Z" />
     </svg>
     """
   end
