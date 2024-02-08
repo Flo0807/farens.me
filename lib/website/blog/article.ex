a10308f3453536c2cad9e69c3af92ce9ab68a7f8
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-08 13:38:44 +0100

    Add heading links to article and render table of contents

--- lib/website/blog/article.ex
+++ lib/website/blog/article.ex
@@ -11,17 +11,21 @@ defmodule Website.Blog.Article do
             description: "",
             body: "",
             read_minutes: 0,
+            heading_links: [],
             published: false
 
   def build(filename, attrs, body) do
     [year, month_day_id] = filename |> Path.rootname() |> Path.split() |> Enum.take(-2)
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
+
     read_minutes = calculate_read_minutes(body)
+    heading_links = parse_headings(body)
 
     struct!(
       __MODULE__,
-      [id: id, date: date, body: body, read_minutes: read_minutes] ++ Map.to_list(attrs)
+      [id: id, date: date, body: body, read_minutes: read_minutes, heading_links: heading_links] ++
+        Map.to_list(attrs)
     )
   end
 
@@ -35,6 +39,32 @@ defmodule Website.Blog.Article do
     case div(word_count, @words_per_minute) do
       0 -> 1
       n -> n
+    end
+  end
+
+  defp parse_headings(body) do
+    body
+    |> Floki.parse_fragment!()
+    |> Enum.reduce([], fn
+      {"h2", _class, child} = el, acc ->
+        acc ++ [%{label: Floki.text(el), href: get_href(child), childs: []}]
+
+      {"h3", _class, child} = el, acc ->
+        List.update_at(acc, -1, fn %{childs: subs} = h2 ->
+          %{h2 | childs: subs ++ [%{label: Floki.text(el), href: get_href(child), childs: []}]}
+        end)
+
+      _other, acc ->
+        acc
+    end)
+  end
+
+  def get_href(heading_element) do
+    attr = heading_element |> Floki.find("a") |> Floki.attribute("href")
+
+    case attr do
+      [] -> nil
+      [href | _] -> href
     end
   end
 end
