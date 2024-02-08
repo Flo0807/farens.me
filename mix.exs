3ea738cba231b4457b8896d11324a682ce0fc6f7
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-08 18:49:06 +0100

    Use own phoenix_seo fork until new release

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Website.MixProject do
       {:yaml_elixir, "~> 2.9.0"},
       {:tailwind_formatter, "~> 0.4.0", only: [:dev, :test], runtime: false},
       {:nimble_publisher, "~> 1.1.0"},
-      {:phoenix_seo, "~> 0.1.8"}
+      {:phoenix_seo, git: "https://github.com/Flo0807/phoenix_seo"}
     ]
   end
 
