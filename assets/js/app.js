2f6cb6d509064f2176851f0efe14f87985277ea9
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-10 07:46:59 +0100

    Increase topbar debounce

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,7 +37,7 @@ let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfTo
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
+window.addEventListener("phx:page-loading-start", _info => topbar.show(500))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
