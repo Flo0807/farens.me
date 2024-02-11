690494550e209fafec3fe59d4128755aa4dcac2c
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-11 12:07:18 +0100

    Format

--- lib/website_web.ex
+++ lib/website_web.ex
@@ -17,98 +17,98 @@ defmodule WebsiteWeb do
   those modules here.
   """
 
-def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
-def router do
-  quote do
-    use Phoenix.Router, helpers: false
+  def router do
+    quote do
+      use Phoenix.Router, helpers: false
 
-    # Import common connection and controller functions to use in pipelines
-    import Plug.Conn
-    import Phoenix.Controller
-    import Phoenix.LiveView.Router
+      # Import common connection and controller functions to use in pipelines
+      import Plug.Conn
+      import Phoenix.Controller
+      import Phoenix.LiveView.Router
+    end
   end
-end
 
-def channel do
-  quote do
-    use Phoenix.Channel
+  def channel do
+    quote do
+      use Phoenix.Channel
+    end
   end
-end
 
-def controller do
-  quote do
-    use Phoenix.Controller,
-      formats: [:html, :json],
-      layouts: [html: WebsiteWeb.Layouts]
+  def controller do
+    quote do
+      use Phoenix.Controller,
+        formats: [:html, :json],
+        layouts: [html: WebsiteWeb.Layouts]
 
-    import Plug.Conn
-    import WebsiteWeb.Gettext
+      import Plug.Conn
+      import WebsiteWeb.Gettext
 
-    unquote(verified_routes())
+      unquote(verified_routes())
+    end
   end
-end
 
-def live_view do
-  quote do
-    use Phoenix.LiveView,
-      layout: {WebsiteWeb.Layouts, :app},
-      container: {:div, class: "h-full"}
+  def live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {WebsiteWeb.Layouts, :app},
+        container: {:div, class: "h-full"}
 
-    unquote(html_helpers())
+      unquote(html_helpers())
+    end
   end
-end
 
-def live_component do
-  quote do
-    use Phoenix.LiveComponent
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
 
-    unquote(html_helpers())
+      unquote(html_helpers())
+    end
   end
-end
 
-def html do
-  quote do
-    use Phoenix.Component
+  def html do
+    quote do
+      use Phoenix.Component
 
-    # Import convenience functions from controllers
-    import Phoenix.Controller,
-      only: [get_csrf_token: 0, view_module: 1, view_template: 1]
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_csrf_token: 0, view_module: 1, view_template: 1]
 
-    # Include general helpers for rendering HTML
-    unquote(html_helpers())
+      # Include general helpers for rendering HTML
+      unquote(html_helpers())
+    end
   end
-end
 
-defp html_helpers do
-  quote do
-    # HTML escaping functionality
-    import Phoenix.HTML
-    # Core UI components and translation
-    import WebsiteWeb.CoreComponents
-    import WebsiteWeb.Gettext
+  defp html_helpers do
+    quote do
+      # HTML escaping functionality
+      import Phoenix.HTML
+      # Core UI components and translation
+      import WebsiteWeb.CoreComponents
+      import WebsiteWeb.Gettext
 
-    # Shortcut for generating JS commands
-    alias Phoenix.LiveView.JS
+      # Shortcut for generating JS commands
+      alias Phoenix.LiveView.JS
 
-    # Routes generation with the ~p sigil
-    unquote(verified_routes())
+      # Routes generation with the ~p sigil
+      unquote(verified_routes())
+    end
   end
-end
 
-def verified_routes do
-  quote do
-    use Phoenix.VerifiedRoutes,
-      endpoint: WebsiteWeb.Endpoint,
-      router: WebsiteWeb.Router,
-      statics: WebsiteWeb.static_paths()
+  def verified_routes do
+    quote do
+      use Phoenix.VerifiedRoutes,
+        endpoint: WebsiteWeb.Endpoint,
+        router: WebsiteWeb.Router,
+        statics: WebsiteWeb.static_paths()
+    end
   end
-end
 
-@doc """
-When used, dispatch to the appropriate controller/view/etc.
-"""
-defmacro __using__(which) when is_atom(which) do
-  apply(__MODULE__, which, [])
-end
+  @doc """
+  When used, dispatch to the appropriate controller/view/etc.
+  """
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
