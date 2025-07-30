aef01b21f496d74b51a78aa62e8558e68e492af3
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-07-30 16:52:32 +0200

    Update esbuild and tailwind config

--- config/config.exs
+++ config/config.exs
@@ -27,9 +27,9 @@ config :esbuild,
   version: "0.25.8",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]},
   ]
 
 # Configure tailwind (the version is required)
@@ -38,7 +38,7 @@ config :tailwind,
   default: [
     args: ~w(
     --input=assets/css/app.css
-    --output=priv/static/assets/app.css
+    --output=priv/static/assets/css/app.css
   ),
     cd: Path.expand("..", __DIR__)
   ]
