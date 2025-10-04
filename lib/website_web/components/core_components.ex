cd54f9220a1074af53e12ed8fa6cce910303a17d
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-10-04 19:11:27 +0200

    Add animation to tags on hover

--- lib/website_web/components/core_components.ex
+++ lib/website_web/components/core_components.ex
@@ -413,7 +413,7 @@ defmodule WebsiteWeb.CoreComponents do
           phx-click="select-tag"
           phx-value-tag={tag}
           class={[
-            "badge badge-neutral cursor-pointer hover:badge-primary",
+            "badge badge-neutral cursor-pointer transition-transform duration-100 hover:scale-105",
             String.downcase(tag) == @search_tag && "badge-primary"
           ]}
         >
