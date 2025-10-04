9bf60123a5ea0fc968f313432cec3597dfe5d26a
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-10-04 17:12:53 +0200

    Add tidewave

--- mix.exs
+++ mix.exs
@@ -75,7 +75,8 @@ defmodule Website.MixProject do
       {:phoenix_live_reload, "== 1.6.1", only: :dev},
       {:tailwind_formatter, "== 0.4.2", only: [:dev, :test], runtime: false},
       {:credo, "== 1.7.12", only: [:dev, :test], runtime: false},
-      {:lazy_html, "== 0.1.8", only: :test}
+      {:lazy_html, "== 0.1.8", only: :test},
+      {:tidewave, "== 0.5.0", only: :dev}
     ]
   end
 
