b191432d58ff7bb161fb21364483877573515f9e
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-12-24 11:00:49 +0100

    Start Playwright supervisor and set evaluate timeouts

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,5 @@
+{:ok, _} = PhoenixTest.Playwright.Supervisor.start_link()
+
 Application.put_env(:phoenix_test, :base_url, WebsiteWeb.Endpoint.url())
 
 ExUnit.configure(exclude: [playwright: true])
