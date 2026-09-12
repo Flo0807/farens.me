843ddf5a0952aeab1907e651b3a3bbb36ba2d8ed
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2026-09-12 11:03:19 +0200

    Simplify personal site page templates

--- lib/website_web/live/home_live/index.ex
+++ lib/website_web/live/home_live/index.ex
@@ -2,14 +2,17 @@ defmodule WebsiteWeb.HomeLive.Index do
   use WebsiteWeb, :live_view
 
   alias Website.Blog
+  alias Website.Projects
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     articles = Blog.all_articles() |> Enum.take(3)
+    projects = Projects.all_projects() |> Enum.take(2)
 
     socket =
       socket
       |> assign(:articles, articles)
+      |> assign(:projects, projects)
       |> assign(:page_title, "Florian Arens - Software Engineer")
       |> assign(:og_image_text, "Florian Arens")
       |> assign(
